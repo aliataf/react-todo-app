85e63e9027fe07a28db491ae5f25d0fe96d217c8
Ali Ataf <aliattaf98@gmail.com>
2021-12-20 15:25:36 -0800

    fix: change app font

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,12 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
+				<h1>// TODO APP</h1>
 			</header>
 		</div>
 	);
